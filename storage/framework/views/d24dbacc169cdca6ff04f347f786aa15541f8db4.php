883338694d9787412a63555bc7d518169dadf309
muhamad4598 <126425361+muhamad4598@users.noreply.github.com>
2023-05-18 03:21:21 +0300

    added

---
+++ storage/framework/views/d24dbacc169cdca6ff04f347f786aa15541f8db4.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='UTF-8'>
+    <title>Log in</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class='container'>
+        <div class='row'>
+            <div class="col-md-4 col-md-offset-4" style="margin-top:30px">
+                <h2>Log in<h2>
+                        <hr>
+                        <div class='form-group'>
+                            <form action="<?php echo e(route('login-user')); ?>" method="post">
+                                <?php if(Session::has('SUCCESS')): ?>
+                                    <div class="alert alert-success"><?php echo e(Session::get('SUCCESS')); ?></div>
+                                <?php endif; ?>
+                                <?php if(Session::has('fail')): ?>
+                                    <div class="alert alert-danger"><?php echo e(Session::get('fail')); ?></div>
+                                <?php endif; ?>
+                                <?php echo csrf_field(); ?>
+                                <div class='form-group'>
+                                    <label for='email'>Email</label>
+                                    <input type='text' class='form-control' value='<?php echo e(old('email')); ?>'
+                                        name='email'>
+                                    <span class="text-danger">
+                                        <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                            <?php echo e($message); ?>
+
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                    </span>
+
+                                </div>
+                                <div class='form-group'>
+                                    <label for='password'>Password</label>
+                                    <input type='text' class='form-control' value='<?php echo e(old('password')); ?>'
+                                        name='password'>
+                                    <span class="text-danger">
+                                        <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                            <?php echo e($message); ?>
+
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                    </span>
+
+                                </div>
+
+                                <button class=' btn btn-block btn-primary' style="margin-top: 20px">log in</button>
+
+                            </form>
+                            <a  style="font-size:13px" href="register">don't have an account register here .</a>
+                        </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+</script>
+
+</html>
+<?php /**PATH C:\Users\nissrin ahmad\Desktop\laravel\securelam\resources\views/auth\login.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d7a6107b78beb04b55483254da663ea73a04e654.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class=" col-md-offset-4" style="margin-top:20px"></div>
+            <h2>
+                Welcome to admin dashboard!!
+            </h2>
+            <ul class="list-group">
+            <li class="list-group-item"><a href="/admin/users">control user</a></li>
+            <li class="list-group-item"><a href="/admin/show-unApproved">waiting user</a></li>
+            <li class="list-group-item"><a href="/dashboard"> my account</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php /**PATH C:\Users\nissrin ahmad\Desktop\laravel\securealm\resources\views/adminDashboard.blade.php ENDPATH**/ ?>
